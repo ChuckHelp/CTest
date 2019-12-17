5889693716483fdd6e0944979fd4034b53bbcd9e
Chuck Lu <chuck.lu@qq.com>
2019-12-17 13:00:36 +0800

    https://www.cnblogs.com/CCC1/p/12053017.html

--- CTest/Main.c
+++ CTest/Main.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main() 
+
+int main()
 {
-	printf("Hello World!\n");
-	system("pause");
+	int max(int x, int y);
+	int a, b, c;
+	scanf("%d %d", &a, &b);
+	c = max(a, b);
+	printf("max=%d\n", c);
 	return 0;
+}
+
+int max(int x, int y)
+{
+	int z;
+	if (x > y) z = y;
+	system("pause");
+	return(z);
 }
